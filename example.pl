f867c5fa56061bb3d7e242391e5bbf13bcd8264d
John Horner <john.horner@gmail.com>
2016-01-14 12:23:43 +1100

    Change to named args

--- example.pl
+++ example.pl
@@ -3,7 +3,9 @@ use strict;
 use warnings;
 use Data::Dumper;
 use Paginate;
-my $p = Paginate->new( { die_on_bad_args => 0 } );
-my $pagination = $p->Paginate( 9, 10, 1 );
+my $p = Paginate->new( die_on_bad_args => 0 );
+my $pagination =
+  $p->Paginate( total_records => 101, per_page => 10, current_page => 9 );
 print Dumper($pagination);
+
 
