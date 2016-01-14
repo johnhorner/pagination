3b2683e84845777962028c442ec8dfe541eeaccf
John Horner <john.horner@gmail.com>
2016-01-14 12:23:10 +1100

    Add pragmas, change args, add warnings, allow page to be both first and last.

--- Paginate.pm
+++ Paginate.pm
@@ -1,42 +1,66 @@
 package Paginate;
+use strict;
+use warnings;
 use POSIX qw(ceil);
-use Data::Dumper::Simple;
 
 sub new {
     my $class       = shift;
-    my $preferences = shift;
+    my %preferences = @_;
     my $self        = {};
-    foreach ( keys( %{$preferences} ) ) { $self->{$_} = $preferences->{$_}; }
+    foreach ( keys(%preferences) ) { $self->{$_} = $preferences{$_} }
     return bless( $self, $class );
 }
 
 sub Paginate {
     my $self = shift();
-    my ( $total_records, $per_page, $current_page ) = @_;
-    my $p = ();
-    $p->{total_records} = $total_records;
-    $p->{per_page}      = defined($per_page) ? $per_page : 10;
-    $p->{current_page}  = defined($current_page) ? $current_page : 1;
-    $p->{total_pages}   = ceil( $total_records / $p->{per_page} );
-    if ( $self->{die_on_bad_args} ) {
+    my %args = @_;
+    my $p    = ();
+    $p->{warnings}      = [];
+    $p->{total_records} = $args{'total_records'};
+    $p->{per_page}      = defined( $args{'per_page'} ) ? $args{'per_page'} : 10;
+    $p->{current_page} =
+      defined( $args{'current_page'} ) ? $args{'current_page'} : 1;
+    $p->{total_pages} = ceil( $args{'total_records'} / $p->{per_page} );
 
-        if ( $p->{current_page} > $p->{total_pages} ) {
+    if ( $p->{total_records} < $p->{per_page} ) {
+        push(
+            @{ $p->{warnings} },
+            'Per-page value is less than the total number of records.'
+        );
+    }
+
+    if ( $p->{current_page} > $p->{total_pages} ) {
+        if ( $self->{die_on_bad_args} ) {
             die("Error: current page greater than total pages");
+        } else {
+            push(
+                @{ $p->{warnings} },
+                'Current page value is greater than total pages.'
+            );
         }
     }
     $p->{record_offset}        = ( $p->{current_page} - 1 ) * $p->{per_page};
     $p->{first_record_on_page} = $p->{record_offset};
     $p->{last_record_on_page}  = $p->{record_offset} + ( $p->{per_page} - 1 );
     if ( $p->{last_record_on_page} >= $p->{total_records} ) {
-        $p->{last_record_on_page} = ($p->{total_records} - 1);
+        $p->{last_record_on_page} = ( $p->{total_records} - 1 );
     }
-    $p->{record_limit} = $per_page;
+    $p->{record_limit} = $args{'per_page'};
     if ( $p->{current_page} == $p->{total_pages} ) {
         $p->{current_is_last} = 1;
     }
-    elsif ( $p->{current_page} == 1 ) { $p->{current_is_first}    = 1; }
-    else                              { $p->{current_is_midrange} = 1; }
+    if ( $p->{current_page} == 1 ) {
+        $p->{current_is_first} = 1;
+    }
+    if ( ( $p->{current_is_first} ) && ( $p->{current_is_last} ) ) {
+        push( @{ $p->{warnings} }, 'Page is both first and last page.' );
+    }
+
+    if ( !( $p->{current_is_first} ) && !( $p->{current_is_last} ) ) {
+        $p->{current_is_midrange} = 1;
+    }
     return $p;
 }
 1;
+
 
